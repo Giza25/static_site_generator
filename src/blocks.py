a344764f64eb016455604d7ed2074ff421b3ef16
Giza25 <dyma125@gmail.com>
2025-05-10 20:28:38 +0400

    Added a block_to_blocktype function that takes a string that is a block of markdown text and returns a type of it Added some tests for it

--- src/blocks.py
+++ src/blocks.py
@@ -1,9 +1,33 @@
 import re
 from textnode import MDTextType, TextNode
 
+from enum import Flag, auto
+
+class BlockType(Flag):
+    PARAGRAPH = auto()
+    HEADING = auto()
+    CODE = auto()
+    QUOTE = auto()
+    UNORDERED_LIST = auto()
+    ORDERED_LIST = auto()
+
 def markdown_to_blocks(markdown: str):
     result = re.split(r"\n{2,}", markdown)
     for i in range(len(result)):
         result[i] = result[i].strip()
         result[i] = result[i].strip('\n')
     return result
+
+def block_to_block_type(block: str):
+    if re.match(r'#{1,6} ', block) and len(block.splitlines()) == 1:
+        return BlockType.HEADING
+    elif len(re.findall(r'^(```)|(```)$', block)) == 2:
+        return BlockType.CODE
+    elif re.fullmatch(r'(> (?:.+)\n?)+', block):
+        return BlockType.QUOTE
+    elif re.fullmatch(r'(- (?:.+)\n?)+', block):
+        return BlockType.UNORDERED_LIST
+    elif re.fullmatch(r'(\d+\. (?:.+)\n?)+', block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 class TestMDToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -24,3 +24,69 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_paragraph(self):
+        md = """
+This is a paragraph of text
+With a new line
+Several actually
+"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+    
+    def test_code(self):
+        md = """
+```
+print('hello world')
+```
+"""
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_quote(self):
+        md = """
+> This is a quote
+> This is a second quote
+"""
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        md = """
+- First item of the list
+- Second item of the list
+- Third item of the list
+"""
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        md = """
+1. This is the first item of the list
+2. This is the second item of the list
+"""
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+    
+    def test_heading_1_hash(self):
+        md = "# This is a header"
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.HEADING)
+    
+    def test_heading_6_hashes(self):
+        md = "###### This is a header"
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertEqual(block_type, BlockType.HEADING)
+    
+    def test_heading_7_hashes(self):
+        md = "####### This is a header"
+        block = markdown_to_blocks(md)
+        block_type = block_to_block_type(block[0])
+        self.assertNotEqual(block_type, BlockType.HEADING)
