2feb59a402dbafa9efe71e0b4d771432db630542
Giza25 <dyma125@gmail.com>
2025-05-14 23:55:41 +0400

    Created a copy_contents function that copies all the files from /static/ directory to the /public/ directory

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-from textnode import TextNode, MDTextType
+from genericpath import isdir
+import os
+import shutil
+
+def copy_contents(path: str = ""):
+    source_path = f"./static/{path}"
+    target_path = f"./public/{path}"
+    if os.path.exists(target_path) and os.path.isdir(target_path):
+        shutil.rmtree(target_path)
+    os.mkdir(target_path)
+    content_list = os.listdir(source_path)
+    for content in content_list:
+        if os.path.isdir(os.path.join(source_path, content)):
+            copy_contents(os.path.join(path, content))
+        else:
+            shutil.copy(os.path.join(source_path, content), os.path.join(target_path, content))
 
 def main():
-    a = TextNode("Hello World", MDTextType.NORMAL_TEXT)
-    b = TextNode("This is Bold", MDTextType.BOLD_TEXT)
-    print(a)
-    print(b)
-    print(a == b)
-
+    copy_contents()
 
 main()
