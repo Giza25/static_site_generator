58043a5ed074d66adce66a6440e6ba653c182f6c
Giza25 <dyma125@gmail.com>
2025-06-07 21:19:25 +0400

    Added a build option for Github Pages: if this project is run through build.sh it creates and/or updates a /docs folder and copies all the files from /static folder if run via main.sh it generates site locally

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
-from genericpath import isfile
 import os
 import shutil
 import re
+import sys
 
 from blocks import markdown_to_html
 
-def copy_contents(path: str = ""):
+def copy_contents(target: str = "public", path: str = ""):
     source_path = f"./static/{path}"
-    target_path = f"./public/{path}"
+    target_path = f"./{target}/{path}"
     if os.path.exists(target_path) and os.path.isdir(target_path):
         shutil.rmtree(target_path)
     os.mkdir(target_path)
     content_list = os.listdir(source_path)
     for content in content_list:
         if os.path.isdir(os.path.join(source_path, content)):
-            copy_contents(os.path.join(path, content))
+            copy_contents(path=os.path.join(path, content))
         else:
             shutil.copy(os.path.join(source_path, content), os.path.join(target_path, content))
 
@@ -25,17 +25,17 @@ def extract_title(text: str):
     title = text.splitlines()[0].rstrip().lstrip("# ")
     return title
 
-def generate_page_recursively(dir_path_content: str, dir_path_destination: str, template_path: str):
+def generate_page_recursively(dir_path_content: str, dir_path_destination: str, template_path: str, base_path: str):
     contents = os.listdir(dir_path_content)
     for content in contents:
         if os.path.isfile(os.path.join(dir_path_content, content)):
             content_name = os.path.splitext(content)
-            generate_page(os.path.join(dir_path_content, content), os.path.join(dir_path_destination, content_name[0] + ".html"), template_path)
+            generate_page(os.path.join(dir_path_content, content), os.path.join(dir_path_destination, content_name[0] + ".html"), template_path, base_path)
         else:
             os.mkdir(os.path.join(dir_path_destination, content))
-            generate_page_recursively(os.path.join(dir_path_content, content), os.path.join(dir_path_destination, content), template_path)
+            generate_page_recursively(os.path.join(dir_path_content, content), os.path.join(dir_path_destination, content), template_path, base_path)
 
-def generate_page(from_path: str, dest_path: str, template_path: str):
+def generate_page(from_path: str, dest_path: str, template_path: str, base_path: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as md_file, open(template_path) as template_file, open(dest_path, "w") as destination_file:
         md = md_file.read()
@@ -45,10 +45,21 @@ def generate_page(from_path: str, dest_path: str, template_path: str):
         contents_html = contents_nodes.to_html()
         template_html = template_html.replace('{{ Title }}', title)
         template_html = template_html.replace('{{ Content }}', contents_html)
+        template_html = template_html.replace('href="/', f"href=\"{base_path}")
+        template_html = template_html.replace('src="/', f"src=\"{base_path}")
         destination_file.write(template_html)
 
 def main():
-    copy_contents()
-    generate_page_recursively("./content", "./public", "./template.html")
+    base_path = "/"
+    content_path = "./content"
+    destination_path = "./public"
+    template_file_path = "./template.html"
+    if len(sys.argv) > 1: 
+        base_path = sys.argv[1]
+        destination_path = "./docs"
+        copy_contents("docs")
+    else:
+        copy_contents()
+    generate_page_recursively(content_path, destination_path, template_file_path, base_path)
 
 main()
